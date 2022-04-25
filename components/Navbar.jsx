c8e57fb0d4f441d26ae48b235ca39c093e8f071a
Hemant <hrsmart2150@gmail.com>
2022-04-25 18:55:58 +0530

    Favicon and some spacing changed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import {
     FaGithub,
     FaLinkedin
 } from 'react-icons/fa';
-import {RiMenu4Line} from 'react-icons/ri'
+import { RiMenu4Line } from 'react-icons/ri'
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
@@ -16,44 +16,44 @@ const Navbar = () => {
     return (
         <header>
             <nav className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-50'>
-                <Link href='/' to='home'  smooth={true} duration={500}>
-                <div className='hemant text-3xl text-pink-600 cursor-pointer'>
-                    Hemant
-                </div>
+                <Link href='/' to='home' smooth={true} duration={500}>
+                    <div className='hemant text-3xl text-pink-600 cursor-pointer'>
+                        Hemant
+                    </div>
                 </Link>
 
                 {/* menu */}
                 <ul className='hidden md:flex'>
                     <Link href='/' to='home' smooth={true} duration={500}>
                         <li className='hover:text-pink-500 duration-300'>
-                                Home
+                            Home
                         </li>
                     </Link>
 
                     <Link href='/' to='about' smooth={true} duration={500}>
                         <li className='hover:text-pink-500 duration-300'>
-                                About
+                            About
                         </li>
                     </Link>
-                    
+
                     <Link href='/' to='skills' smooth={true} duration={500}>
                         <li className='hover:text-pink-500 duration-300'>
-                                Skills
+                            Skills
                         </li>
                     </Link>
-                    
+
                     <Link href='/' to='work' smooth={true} duration={500}>
                         <li className='hover:text-pink-500 duration-300'>
-                                Work
+                            Work
                         </li>
                     </Link>
-                    
+
                     <Link href='/' to='contact' smooth={true} duration={500}>
                         <li className='hover:text-pink-500 duration-300'>
-                                Contact
+                            Contact
                         </li>
                     </Link>
-                    
+
                 </ul>
 
                 {/* Hamburger */}
@@ -99,26 +99,26 @@ const Navbar = () => {
                 {/* Social icons */}
                 <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                     <ul>
-                        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                            <a href='https://www.linkedin.com/in/hemant0000' rel="noreferrer" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                        <a href='https://www.linkedin.com/in/hemant0000' rel="noreferrer" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                                 Linkedin <FaLinkedin size={30} />
-                            </a>
-                        </li>
-                        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                            <a href='https://github.com/Hemant-0000' rel="noreferrer" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                            </li>
+                        </a>
+                        <a href='https://github.com/Hemant-0000' rel="noreferrer" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                                 Github <FaGithub size={30} />
-                            </a>
-                        </li>
-                        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-                            <a href='https://hrsmart2150@gmail.com' rel="noreferrer" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                            </li>
+                        </a>
+                        <a href="mailto:hrsmart2150@gmail.com" rel="noreferrer" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
                                 Email <HiOutlineMail size={30} />
-                            </a>
-                        </li>
-                        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                            <a href='https://drive.google.com/file/d/17oMHUCGmtIlY7ojOIsQx2MqbfoJmJ_-S/view?usp=sharing' rel="noreferrer" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                            </li>
+                        </a>
+                        <a href='https://drive.google.com/file/d/17oMHUCGmtIlY7ojOIsQx2MqbfoJmJ_-S/view?usp=sharing' rel="noreferrer" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                                 Resume <BsFillPersonLinesFill size={30} />
-                            </a>
-                        </li>
+                            </li>
+                        </a>
                     </ul>
                 </div>
             </nav>
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -17,7 +17,7 @@ import Linux from '../public/linux.png';
 
 const Skills = () => {
     return (
-        <div name='skills' className='w-full pt-[480px] pb-[480px] sm:pt-0 sm:pb-0 h-screen bg-[#0a192f] text-gray-300'>
+        <div name='skills' className='w-full py-10 md:py-20 bg-[#0a192f] text-gray-300'>
             {/* Container */}
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
