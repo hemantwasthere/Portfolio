0f5194f0960546ed72e01ef12d4a8d8bcd3f2999
Hemant <hrsmart2150@gmail.com>
2022-07-12 09:10:39 +0530

    Changed favicon and added 1 more project

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// import WorkImg from '../public/workImg.jpeg';
-// import realEstate from '../public/realestate.jpg';
 import Link from 'next/link';
 
 const Work = () => {
@@ -111,6 +109,32 @@ const Work = () => {
                                     </a>
                                 </Link>
                                 <Link passHref href='https://github.com/Hemant-0000/Image-Gallery'>
+                                    <a target='_blank'>
+                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                            Code
+                                        </button>
+                                    </a>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Grid Item */}
+                    <div className="shadow-lg bg-[url('../public/Netflix.png')] shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div ">
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React JS Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <Link passHref href='https://netflix-by-me.netlify.app/'>
+                                    <a target='_blank'>
+                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                            Demo
+                                        </button>
+                                    </a>
+                                </Link>
+                                <Link passHref href='https://github.com/Hemant-0000/Netflix-Klone'>
                                     <a target='_blank'>
                                         <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                                             Code
